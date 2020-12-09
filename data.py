be6737fb72afe0397c2957cf35f4cef22dcba0de
Ilya Sachok <isachek2001@gmail.com>
2020-12-09 14:44:06 +0300

    Added validation generator

--- data.py
+++ data.py
@@ -53,8 +53,8 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
     if you want to visualize the results of generator, set save_to_dir = "your path"
     '''
-    image_datagen = ImageDataGenerator(**aug_dict)
-    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_datagen = ImageDataGenerator(validation_split=0.2, **aug_dict)
+    mask_datagen = ImageDataGenerator(validation_split=0.2, **aug_dict)
     image_generator = image_datagen.flow_from_directory(
         train_path,
         classes = [image_folder],
@@ -63,6 +63,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
         target_size = target_size,
         batch_size = batch_size,
         save_to_dir = save_to_dir,
+        subset="training",
         save_prefix  = image_save_prefix,
         seed = seed)
     mask_generator = mask_datagen.flow_from_directory(
@@ -74,6 +75,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
         batch_size = batch_size,
         save_to_dir = save_to_dir,
         save_prefix  = mask_save_prefix,
+        subset="training",
         seed = seed)
     train_generator = zip(image_generator, mask_generator)
     for (img,mask) in train_generator:
@@ -82,7 +84,7 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
 
 
 
-def testGenerator(test_path,num_image = 400,target_size = (256,256),flag_multi_class = False,as_gray = True):
+def testGenerator(test_path,num_image = 412, target_size = (256,256),flag_multi_class = False,as_gray = True):
     for i in range(num_image):
         img = io.imread(os.path.join(test_path,"%d.bmp"%i),as_gray = as_gray)
         img = img / 255
@@ -90,6 +92,38 @@ def testGenerator(test_path,num_image = 400,target_size = (256,256),flag_multi_c
         img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
         img = np.reshape(img,(1,)+img.shape)
         yield img
+
+def validationGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
+                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+    image_datagen = ImageDataGenerator(validation_split=0.2, **aug_dict)
+    mask_datagen = ImageDataGenerator(validation_split=0.2, **aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        subset="validation",
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        subset="validation",
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    for (img,mask) in train_generator:
+        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+        yield (img,mask)
 
 
 def geneTrainNpy(image_path,mask_path,flag_multi_class = False,num_class = 2,image_prefix = "image",mask_prefix = "mask",image_as_gray = True,mask_as_gray = True):
